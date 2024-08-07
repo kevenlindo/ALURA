cdfdcc9d4106496035f12b5d2adc6464130d726e
kevenlindo <147113489+kevenlindo@users.noreply.github.com>
2024-08-06 21:13:34 -0300

    06/08/2024

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "NA ESCOLA UM JOVEM ESTAVA PRATICANDO ESPORTES, ELE QUEBROU A PERNA",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "ISTO É MUITO RUIM ",
+                afirmacao: "POR CONTA DO JOVEM QUEBRAR A PERNA ISSO SE TORNA MAL. "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "ELE FAZ ESPORTES QUE BOM ",
+                afirmacao: "."
             }
         ]
     },
